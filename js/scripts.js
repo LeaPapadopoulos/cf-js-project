77fff9c8a821a079629a5ec7ff964bb238f1fde5
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2022-06-16 12:23:50 +0200

    added pokemon objects  in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert("Hello world");
+let pokemonList = [
+  { name: "Bulbasaur", hight: 7, types: ["grass", "poison"] },
+  { name: "Butterfree", hight: 1.1, types: ["bug", "flying"] },
+  { name: "Pikachu", hight: 1, types: ["electric", "ice"] },
+];
+console.log (pokemonList[0]);
 
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
