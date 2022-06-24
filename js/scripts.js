7741248b719d995b80657baccb7a6c161638ac40
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2022-06-24 14:15:40 +0200

    dynamically add pockemon names to grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 let pokemonList = [
-  { name: "Bulbasaur", hight: 7, types: ["grass", "poison"] },
-  { name: "Butterfree", hight: 1.1, types: ["bug", "flying"] },
-  { name: "Pikachu", hight: 1, types: ["electric", "ice"] },
-];
-console.log (pokemonList[0]);
+  { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
+  { name: "Butterfree", height: 3, types: ["bug", "flying"] },
+  { name: "Pikachu", height: 1, types: ["electric", "ice"] },
 
+  //   { name: "Pikachu2", height: 22, types: ["electric", "ice"] },
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemonDisplay = document.querySelector(`#pokemon-${i}`);
+  pokemonDisplay.innerHTML = pokemonList[i].name;
+  if (pokemonList[i].height < 10 && pokemonList[i].height >= 7) {
+    pokemonDisplay.innerHTML = pokemonList[i].name + " Wow, that’s big!";
+  } else {
+    pokemonDisplay.innerHTML = pokemonList[i].name ;
+  }
+}
+
+// {
+//   if (pokemonList[i].height < 10 && pokemonList[i].height >= 7) {
+//     console.log(pokemonList[i].name + " is a big pokemon");
+//   } else if (pokemonList[i].height > 1 && pokemonList[i].height < 7) {
+//     console.log(pokemonList[i].name + " is a small pokemon");
+//   } else {
+//     console.log(pokemonList[i].name + " is a pokemon");
+//   }
+// }
